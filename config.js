7ae75e40d82b10e193d86c242a9e6d65fe5ef3f3
GiangTa2302 <tagiang2001thi@gmail.com>
2022-07-09 21:07:25 +0700

    add

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thảo Mít';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100010339236621';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Happy Birthday';
+const contentCard = 'Chúc Thảo sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Thảo trong hôm nay và cả những ngày sau <3!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thao2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
